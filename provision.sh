f3c2222674677677ed54e68f464c7d062d925a83
Jeff Bowman <jeff@appnovation.com>
2018-04-11 22:07:23 -0500

    Add roles to provisioning vagrant for handling mule interviews

--- provision.sh
+++ provision.sh
@@ -6,28 +6,23 @@ echo 'KEYMAP=us' > /etc/vconsole.conf
 /usr/bin/reflector -c 'United States' -p http --save /etc/pacman.d/mirrorlist
 
 # preinstall dependencies for aur packages coming next
-/usr/bin/pacman -S ansible yajl perl-error git expac nodejs --noconfirm --needed
+/usr/bin/pacman -S base-devel ansible git pacutils perl perl-libwww perl-term-ui perl-json perl-data-dump perl-lwp-protocol-https perl-term-readline-gnu perl-json-xs --noconfirm --needed
 
 if [ ! -d /vagrant/provision-files/library ]; then
-    git clone https://git.project-insanity.org/onny/ansible-pacaur.git /vagrant/provision-files/library
-    mv /vagrant/provision-files/library/packer /vagrant/provision-files/library/pacaur
+    git clone https://github.com/kewlfft/ansible-aur.git /vagrant/provision-files/library
+    ln -s /vagrant/provision-files/library/aur.py /vagrant/provision-files/library/aur
 fi
 
 # install pacaur, needed for installing several packages from AUR repo
 sudo -u vagrant mkdir -p /home/vagrant/aur
 cd /home/vagrant/aur
-sudo -u vagrant /usr/bin/curl https://aur.archlinux.org/cgit/aur.git/snapshot/cower-git.tar.gz -o cower.tar.gz
-sudo -u vagrant /usr/bin/curl https://aur.archlinux.org/cgit/aur.git/snapshot/pacaur.tar.gz -o pacaur.tar.gz
-sudo -u vagrant /usr/bin/tar -zxf cower.tar.gz && cd cower-git
+sudo -u vagrant /usr/bin/curl https://aur.archlinux.org/cgit/aur.git/snapshot/trizen.tar.gz -o trizen.tar.gz
+sudo -u vagrant /usr/bin/tar -zxf trizen.tar.gz && cd trizen
 sudo -u vagrant /usr/bin/makepkg -sc
-/usr/bin/pacman -U cower*.xz --noconfirm --needed
-cd ../
-rm -fr cower-git
-sudo -u vagrant tar -zxf pacaur.tar.gz && cd pacaur
-sudo -u vagrant /usr/bin/makepkg -sc
-/usr/bin/pacman -U pacaur*.xz --noconfirm --needed
-cd ../
-rm -fr pacaur
+/usr/bin/pacman -U trizen*.xz --noconfirm --needed
+#cd ../
+# rm -fr trizen
+# rm trizen.tar.gz
 
 # running from inside vagrant, so full path from that perspective
 # moved to Vagrantfile
